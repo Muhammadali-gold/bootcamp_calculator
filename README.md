0456f82c6492ea88f69662e24a27e70c9103507d
Muhammadali-gold <muhammadali.nuriddinov2000@gmail.com>
2025-07-15 20:25:29 +0500

    Random dice version 2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 version 1.0
 
 random dice
+
+version 2.0
+
+win computer with guessing random dice
+
+version 2.1
+
+show attemps count after win
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length > 0 && Objects.equals(args[0], "random")){
-            System.out.println("Random dice: "+ (int) (Math.floor(1 + Math.random() * 6)));
+        if (args.length > 0 && Objects.equals(args[0], "guess_random")){
+            int attempts = 0;
+            while (true){
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Guess :");
+                String guess = scanner.nextLine();
+
+                if (guess.equals("exit")){
+                    System.out.println("Good bye!");
+                    break;
+                }
+
+                int random_dice = (int) Math.floor(1 + Math.random() * 6);
+                if (Integer.parseInt(guess) == random_dice){
+                    System.out.println("You win! You attempted " + attempts + " times.");
+                    System.out.println("Good bye!");
+                    break;
+                } else {
+                    System.out.println("You lost! Random dice: "+random_dice);
+                    attempts++;
+                }
+            }
         }
     }
 }
